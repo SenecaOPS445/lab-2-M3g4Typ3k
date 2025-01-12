10386b3bbf6f456858d733a9597c53596a143bae
Jakob Tuma <jtuma@myseneca.ca>
2025-01-12 14:56:41 -0500

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Jakob Tuma
+# Author ID: jtuma
+# Date Created: 2025/01/12
+import sys
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
