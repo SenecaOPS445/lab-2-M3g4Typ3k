23fe525c2ba403ebadc01d2087db4daa4fbdb532
Jakob Tuma <jtuma@myseneca.ca>
2025-01-12 14:48:47 -0500

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Author: Jakob Tuma
+# Author ID: jtuma
+# Date Created: 2025/01/12
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
